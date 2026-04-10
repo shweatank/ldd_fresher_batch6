d3f5974c642100997a4dec19e194c6ef2c25260d
harsha123208 <harshavenugurram@mirafra.com>
2026-04-10 15:04:35 +0530

    Add files via upload

---
+++ day1/suming_driver.c
@@ -0,0 +1,86 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/fs.h>
+#include<linux/uaccess.h>
+#include<linux/slab.h>
+#include<linux/vmalloc.h>
+#define DEVICE_NAME "my char"
+#define BUFF_SIZE 256
+static int major_number;
+static char kernel_buffer[256];
+static int buffer_size;
+static int len;
+static int*ptr=NULL;
+static int sum=0;
+static int basic_open(struct inode *ionde,struct file *file)
+{
+ printk(KERN_INFO "baisc_char:device opened in my program\n");
+ return 0;
+}
+static int basic_release(struct inode *inode,struct file *file)
+{
+ printk(KERN_INFO"basic_char:device closed\n");
+ return 0;
+}
+static ssize_t basic_read(struct file *file,char __user *user_buffer,size_t count,loff_t *offset)
+{
+ int bytes_to_copy;
+ int i,j,temp;
+
+ if(*offset >=buffer_size)
+  return 0;
+  int *p=(int*)kernel_buffer;
+
+  bytes_to_copy =min(count,(size_t)(buffer_size - *offset));
+  sprintf(kernel_buffer,"%d",sum);
+ if(copy_to_user(user_buffer,kernel_buffer+ *offset,bytes_to_copy))
+  return -EFAULT;
+
+  *offset +=bytes_to_copy;
+  printk(KERN_INFO"basic_char:read %d bytes  \n",bytes_to_copy);
+  return bytes_to_copy;
+}
+static ssize_t basic_write(struct file* file,const char __user *user_buffer,size_t count,loff_t *offset)
+{
+  int bytes_to_copy,num;
+  bytes_to_copy=min(count,(size_t)BUFF_SIZE);
+  if(copy_from_user(kernel_buffer,user_buffer,bytes_to_copy))
+  return -EFAULT;
+  sscanf(kernel_buffer,"%d",&num);
+  sum=sum+num;
+  buffer_size=bytes_to_copy;
+  printk(KERN_INFO"basic_char:wrote %d bytes data %s len %d\n",bytes_to_copy,kernel_buffer,len);
+ return bytes_to_copy;
+}
+  static struct file_operations basic_fops = {
+ .owner =THIS_MODULE,
+ .open=basic_open,
+ .read =basic_read,
+ .write =basic_write,
+ .release=basic_release,
+};
+ static int __init basic_char_init(void)
+{
+   major_number =register_chrdev(0,DEVICE_NAME,&basic_fops);
+   if(major_number<0)
+  {
+   printk(KERN_ERR "basic_char:failed to register device\n");
+   return major_number;
+  }
+ printk(KERN_INFO "basic_char:loaded\n");
+ printk(KERN_INFO "basic_char:major number=%d\n",major_number);
+ printk(KERN_INFO "Create device node with:\n");
+ printk(KERN_INFO "mknod /dev/%s c %d 0\n",DEVICE_NAME,major_number);
+ return 0;
+}
+static void __exit basic_char_exit(void)
+{  
+ unregister_chrdev(major_number,DEVICE_NAME);
+ printk(KERN_INFO "basi_char:unloded\n");
+}
+module_init(basic_char_init);
+module_exit(basic_char_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("harsha");
+MODULE_DESCRIPTION("educational basic character driver with file operations");
