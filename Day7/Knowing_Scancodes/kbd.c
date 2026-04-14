0188a2d36a8134bcfc1f79eb96655776c2cdd645
rahulthummamirafra <rahulthumma@mirafra.com>
2026-04-14 09:26:51 +0530

    Add files via upload

---
+++ Day7/Knowing_Scancodes/kbd.c
@@ -0,0 +1,37 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/interrupt.h>
+#include<linux/io.h>
+#define KBD_IRQ 1
+#define KBD_DATA_PORT 0x60
+static irqreturn_t keyboard_interrupt(int irq,void *dev_id){
+  unsigned char scancode;
+  //Read scan code from keyboard data port
+  scancode = inb(KBD_DATA_PORT);
+  printk(KERN_INFO "keyboard IRQ: Scan code =0x%x\n",scancode);
+  return IRQ_HANDLED;
+}
+static int __init kbd_driver_init(void){
+  int result;
+  printk(KERN_INFO "Keyboard Driver Loaded\n");
+  //request IRQ 1(keyboard)
+  result = request_irq(KBD_IRQ,keyboard_interrupt,IRQF_SHARED,"kbd_driver",(void *)(keyboard_interrupt));
+
+  if(result){
+    printk(KERN_ERR "Cannot register IRQ 1\n");
+    return result;
+  }
+  return 0;
+}
+static void __exit kbd_driver_exit(void){
+   free_irq(KBD_IRQ,(void *)(keyboard_interrupt));
+   printk(KERN_INFO "Keyboard Driver Unloaded\n");
+}
+
+module_init(kbd_driver_init);
+module_exit(kbd_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rahul");
+MODULE_DESCRIPTION("Simple Keyboard Interrupt Driver");
