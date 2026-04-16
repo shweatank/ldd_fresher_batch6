6160d9a0485cfcfa42369b3fda7a4465c2e52931
Shaik Almas <shaikalmas@mirafra.com>
2026-04-16 16:21:27 +0530

    Add files via upload

---
+++ procfs/procfs_demo.mod.c
@@ -0,0 +1,72 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x546c19d9, "validate_usercopy_range" },
+	{ 0xa61fd7aa, "__check_object_size" },
+	{ 0x092a35a2, "_copy_from_user" },
+	{ 0x90a48d82, "__ubsan_handle_out_of_bounds" },
+	{ 0x9479a1e8, "strnlen" },
+	{ 0xe54e0a6b, "__fortify_panic" },
+	{ 0x437e81c7, "simple_read_from_buffer" },
+	{ 0xa2a6a253, "remove_proc_entry" },
+	{ 0xd272d446, "__fentry__" },
+	{ 0x8cd5468a, "proc_create" },
+	{ 0xe8213e80, "_printk" },
+	{ 0xd272d446, "__x86_return_thunk" },
+	{ 0x4749ded2, "module_layout" },
+};
+
+static const u32 ____version_ext_crcs[]
+__used __section("__version_ext_crcs") = {
+	0x546c19d9,
+	0xa61fd7aa,
+	0x092a35a2,
+	0x90a48d82,
+	0x9479a1e8,
+	0xe54e0a6b,
+	0x437e81c7,
+	0xa2a6a253,
+	0xd272d446,
+	0x8cd5468a,
+	0xe8213e80,
+	0xd272d446,
+	0x4749ded2,
+};
+static const char ____version_ext_names[]
+__used __section("__version_ext_names") =
+	"validate_usercopy_range\0"
+	"__check_object_size\0"
+	"_copy_from_user\0"
+	"__ubsan_handle_out_of_bounds\0"
+	"strnlen\0"
+	"__fortify_panic\0"
+	"simple_read_from_buffer\0"
+	"remove_proc_entry\0"
+	"__fentry__\0"
+	"proc_create\0"
+	"_printk\0"
+	"__x86_return_thunk\0"
+	"module_layout\0"
+;
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "40E64D6926D7C4D8CFCCC37");
