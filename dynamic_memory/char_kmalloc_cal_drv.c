d0230053b2c90848d2ab82067b4e2d650a1853be
satish kothapalli <satishkothapalli@mirafra.com>
2026-04-17 14:29:16 +0530

    dynamic_memory

---
+++ dynamic_memory/char_kmalloc_cal_drv.c
@@ -0,0 +1,152 @@
+#include<linux/module.h>   //core module macros
+#include<linux/kernel.h>   //printk()
+#include<linux/init.h>     //__init,__exit
+#include<linux/fs.h>       //register _chrdev,file_operations
+#include<linux/uaccess.h> //copy_to_user,copy_from_user
+
+#define DEVICE_NAME "basic_char"
+//#define BUF_SIZE 256
+
+static int major_number;
+//static char kernel_buffer[BUF_SIZE];
+static int buffer_size;
+static char *kernel_buffer=NULL;
+
+/* 
+ * called when user opens /dev/basic_char
+ */
+static int basic_open(struct inode *inode,struct file *file)
+{
+	printk(KERN_INFO "basic_char:device opened\n");
+	return 0;
+}
+/*
+ * called when user closes /dev/basic_char
+ */
+static int basic_release(struct inode *inode,struct file *file)
+{
+	printk(KERN_INFO "basic_char:device closed\n");
+	return 0;
+}
+/*
+ * called when user reads from /dev/basic_char
+ */
+static ssize_t basic_read(struct file *file,char __user *user_buffer,size_t count,loff_t *offset)
+{
+	int bytes_to_copy;
+	/* 
+	 * If offset is beyond data,return 0(EOF)
+	 */
+	if(*offset>=buffer_size)
+		return 0;
+	bytes_to_copy=min(count,(size_t)(buffer_size - *offset));
+	/*
+	 * copy data from kernel space to user space
+	 */
+	if(copy_to_user(user_buffer,kernel_buffer+*offset,bytes_to_copy))
+		return -EFAULT;
+	*offset +=bytes_to_copy;
+	printk(KERN_INFO "In read function");
+	printk(KERN_INFO "basic_char: read %d bytes\n",bytes_to_copy);
+	kfree(kernel_buffer);
+	return bytes_to_copy;
+}
+/*
+ * called when user writes to dev/basic_char
+ */
+static ssize_t basic_write(struct file *file,const char __user *user_buffer,size_t count,loff_t *offset)
+{
+	int bytes_to_copy;
+	bytes_to_copy=count;
+	buffer_size=bytes_to_copy;
+	/*
+	 * copy data from user space to kernel space
+	 */
+	kernel_buffer=(char *)kmalloc(bytes_to_copy*sizeof(char),GFP_KERNEL);
+	if(copy_from_user(kernel_buffer,user_buffer,bytes_to_copy))
+	{
+		return -EFAULT;
+	}
+	printk(KERN_INFO "In write function");
+	printk(KERN_INFO "basic_char:wrote %d bytes\n",bytes_to_copy);
+
+	int i=0,op1=0,op2=0,res=0;
+	while(kernel_buffer[i])
+	{
+		if(kernel_buffer[i]==',')
+		{
+			i++;
+			while(kernel_buffer[i]!=',')
+			{
+				op2=op2*10+(kernel_buffer[i]-'0');
+				i++;
+			}
+			i++;
+			break;
+
+		}
+		op1=op1*10+kernel_buffer[i]-'0';
+		i++;
+	}
+	if(kernel_buffer[i]=='A')
+		res=op1+op2;
+	else if(kernel_buffer[i]=='S')
+		res=op1-op2;
+	else if(kernel_buffer[i]=='M')
+		res=op1*op2;
+	else if(kernel_buffer[i]=='D')
+		res=op1/op2;
+	else
+		printk(KERN_INFO "Invlaid operation");
+	printk(KERN_INFO "%d\n",res);
+	sprintf(kernel_buffer,"%d",res);
+
+	return bytes_to_copy;
+}
+/*
+ * file operation structure
+ * This connects system calls to driver functions 
+ */
+static struct file_operations basic_fops={
+	.owner=THIS_MODULE,
+	.open=basic_open,
+	.read=basic_read,
+	.write=basic_write,
+	.release=basic_release,
+};
+/*
+ * Module initialization
+ */
+static int __init basic_char_init(void)
+{
+	/*
+	 * Register character device
+	 * 0->dynamic major number
+	 */
+	major_number=register_chrdev(0,DEVICE_NAME,&basic_fops);
+	if(major_number<0)
+	{
+		printk(KERN_ERR "basic_char:failed to register device\n");
+		return major_number;
+	}
+	printk(KERN_INFO "basic_char: loaded\n");
+	printk(KERN_INFO "basic_char: major_number=%d\n",major_number);
+	printk(KERN_INFO "Create device node with:\n");
+	printk(KERN_INFO "mknod /dev/%s c %d 0\n",DEVICE_NAME,major_number);
+	return 0;
+}
+/*
+ * module cleanup
+ */
+static void __exit basic_char_exit(void)
+{
+	unregister_chrdev(major_number,DEVICE_NAME);
+	printk(KERN_INFO "basic_char:unloaded\n");
+}
+//kernel module macros
+module_init(basic_char_init);
+module_exit(basic_char_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Satish");
+MODULE_DESCRIPTION("Educational basic character driver with file operations");
---
+++ dynamic_memory/char_kmalloc_cal_user.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<fcntl.h>
+int main()
+{
+	int fd=open("/dev/char_kmalloc_cal_drv",O_WRONLY);
+	if(fd<0)
+	{
+		perror("open");
+		exit(0);
+	}
+	char str[100];
+	printf("Enter the data\n");
+	fgets(str,100,stdin);
+	str[strlen(str)-1]='\0';
+	int n;
+	if((n=write(fd,str,strlen(str))<0))
+	{
+		perror("write");
+		exit(0);
+	}
+	close(fd);
+
+	char ptr[100];
+	fd=open("/dev/char_kmalloc_cal_drv",O_RDONLY);
+	read(fd,ptr,sizeof(ptr));
+	printf("The result is %s\n",ptr);
+
+
+
+}
