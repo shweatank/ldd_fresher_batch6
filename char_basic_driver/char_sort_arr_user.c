1785885bbcf269453b705b157da34078f5e1c89a
satish kothapalli <satishkothapalli@mirafra.com>
2026-04-17 12:24:01 +0530

    Add folder

---
+++ char_basic_driver/char_sort_arr_user.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main()
+{
+	int fd=open("/dev/char_sort_arr_drv",O_WRONLY);
+	if(fd<0)
+	{
+		perror("open");
+		exit(0);
+	}
+	printf("File is opened successfully\n");
+	int n,ele;
+	printf("Enter the number of elements\n");
+	scanf("%d",&ele);
+	int *ptr=malloc(ele*sizeof(int));
+	printf("Enter the elements\n");
+	for(int i=0;i<ele;i++)
+	{
+		scanf("%d",&ptr[i]);
+	}
+	printf("\n");
+
+	if(((n=write(fd,ptr,ele*sizeof(int)))<0))
+	{
+		perror("Write");
+		exit(0);
+	}
+	printf("send\n");
+	close(fd);
+
+	fd=open("/dev/char_sort_arr_drv",O_RDONLY);
+	if(fd<0)
+	{
+		perror("open");
+		exit(0);
+	}
+	printf("FIle is opened successfully\n");
+	if(((n=read(fd,ptr,ele*sizeof(int))<0)))
+	{
+		perror("read");
+		exit(0);
+	}
+	for(int i=0;i<ele;i++)
+	{
+		printf("%d ",ptr[i]);
+	}
+	printf("\n");
+
+}
+
---
+++ char_basic_driver/char_str_rev_user.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main()
+{
+	int fd=open("/dev/char_str_rev_drv",O_WRONLY);
+	if(fd<0)
+	{
+		perror("open");
+		exit(0);
+	}
+	printf("File is opened successfully\n");
+	char buf[100];
+	printf("Enter a string\n");
+	fgets(buf,100,stdin);
+	buf[strlen(buf)-1]='\0';
+	int n;
+	if((n=write(fd,buf,strlen(buf)))<0)
+	{
+		perror("Write");
+		exit(0);
+	}
+	printf("send\n");
+	close(fd);
+
+	fd=open("/dev/char_str_rev_drv",O_RDONLY);
+	if(fd<0)
+	{
+		perror("open");
+		exit(0);
+	}
+	printf("FIle is opened successfully\n");
+	if((n=read(fd,buf,sizeof(buf)))<0)
+	{
+		perror("read");
+		exit(0);
+	}
+	printf("received %s\n",buf);
+}
+
