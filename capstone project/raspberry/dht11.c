8c5d80339e355f34908a2bb20030eb59e0d79544
Shaik Almas <shaikalmas@mirafra.com>
2026-05-25 12:24:55 +0530

    Update dht11.c

--- capstone project/raspberry/dht11.c
+++ capstone project/raspberry/dht11.c
@@ -100,6 +100,8 @@ int dht11_read_data(int *temp, int *hum)
         data[i / 8] <<= 1;
 
         // Long HIGH pulse means bit = 1
+        //High pulse > 40 -> bit = 1;
+        //High pulse < 40 -> bit = 0;
         if (ht > 40)
             data[i / 8] |= 1;
     }
